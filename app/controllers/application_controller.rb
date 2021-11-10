43351ba51ce830ee1e12c73daf39bb798022be28
luandang4 <luandang4@gmail.com>
2021-11-10 20:48:50 +0700

    Chapter 9: Advanced login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "Hello world"
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_page#home"
+    get "/help", to: "static_page#help"
+    get "/about", to: "static_page#about"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    resources :users, only: %i(new show create)
+  end
 end
