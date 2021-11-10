0e6a126de308db4b547c75c8e2b6c7eec359f610
luandang4 <luandang4@gmail.com>
2021-11-10 20:48:50 +0700

    Chapter 12: Reset password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,69 +1,73 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
-  # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
-  # GET /users/1 or /users/1.json
-  def show
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user.present?
+
+    flash[:danger] = t "flash.danger"
+    redirect_to login_url
   end
 
-  # POST /users or /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t "flash.info"
+      redirect_to root_url
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      flash[:success] = t "flash.profile_updated"
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
-  # DELETE /users/1 or /users/1.json
+  def edit; end
+
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
+    user = User.find_by(id: params[:id])
+    if user&.destroy
+      flash[:success] = t "flash.user_deleted"
+    else
+      flash[:danger] = t "flash.delete_fail"
     end
+    redirect_to users_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+  def user_params
+    params
+      .require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "flash.login_warning"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_page#home"
+    get "/help", to: "static_page#help"
+    get "/about", to: "static_page#about"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    resources :users
+    resources :account_activations, only: [:edit]
+    resources :password_resets,     only: [:new, :create, :edit, :update]
+  end
 end
