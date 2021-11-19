dfa9de3d850c2917a0a5a3dd531422379628f2df
luandang4 <luandang4@gmail.com>
2021-11-19 10:46:10 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
       flash[:success] = t "flash.login.successed"
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = t "flash.invalid"
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -22,10 +29,45 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      flash[:success] = t "flash.profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    if user&.destroy
+      flash[:success] = t "flash.user_deleted"
+    else
+      flash[:danger] = t "flash.delete_fail"
+    end
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params
       .require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "flash.login_warning"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by(id: params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
